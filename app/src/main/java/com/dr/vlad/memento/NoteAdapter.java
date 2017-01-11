082c35c544fcd150d7d9e3f88933244b1209aa30
vlad-d <drinceanu.vlad@gmail.com>
2017-01-11 23:58:08 +0200

    Item note

--- app/src/main/java/com/dr/vlad/memento/NoteAdapter.java
+++ app/src/main/java/com/dr/vlad/memento/NoteAdapter.java
@@ -1,7 +1,12 @@
 package com.dr.vlad.memento;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.dr.vlad.memento.notes.Note;
+import com.dr.vlad.memento.notes.NoteItem;
 
 import java.util.List;
 
@@ -33,8 +39,29 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(NoteAdapter.ViewHolder holder, int position) {
         final Note note = notes.get(position);
+        String title = note.getTitle();
+        if (!title.isEmpty()) {
+            TextView tvTitle = createTextView(false);
+            tvTitle.setText(title);
+            tvTitle.setTypeface(null, Typeface.BOLD);
+            holder.layout.addView(tvTitle);
+        }
+
+        List<NoteItem> items = note.getItems();
+        if (!items.isEmpty()) {
+            for (NoteItem item : items) {
+                String text = item.getText();
+                if (!text.isEmpty()) {
+                    TextView tvItem = createTextView(true);
+                    tvItem.setText(text);
+                    holder.layout.addView(tvItem);
+                }
+
+            }
+        }
+
 
     }
 
@@ -43,17 +70,38 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
         return notes.size();
     }
 
-    private void createEditText(boolean drawLine) {
+    private TextView createTextView(boolean drawLine) {
         TextView textView = new TextView(context);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        params.setMargins(16, 16, 16, 0);
+        params.setMargins(24, 24, 24, 24);
         textView.setLayoutParams(params);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+        textView.setTextColor(ContextCompat.getColor(context, R.color.colorTextSecondary));
+
+        if (drawLine) {
+            textView.setPaintFlags(textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        }
+        return textView;
+    }
+
+    private TextView createLabelTextView(int color) {
+        TextView textView = new TextView(context);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0, 8, 0, 0);
+        textView.setLayoutParams(params);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+        textView.setBackgroundColor(color);
+
+        return textView;
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
+
+        LinearLayout layout;
+
         public ViewHolder(View itemView) {
             super(itemView);
-
+            layout = (LinearLayout) itemView.findViewById(R.id.ll_note_item);
 
         }
     }
