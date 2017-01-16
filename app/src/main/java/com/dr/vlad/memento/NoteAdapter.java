f1dfe5724391710880c8ac7cb116963e7597a406
vlad-d <drinceanu.vlad@gmail.com>
2017-01-16 08:04:20 +0200

    note adapter updated

--- app/src/main/java/com/dr/vlad/memento/NoteAdapter.java
+++ app/src/main/java/com/dr/vlad/memento/NoteAdapter.java
@@ -1,24 +1,18 @@
 package com.dr.vlad.memento;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Typeface;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.dr.vlad.memento.notes.Note;
 import com.dr.vlad.memento.notes.NoteItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -42,14 +36,29 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
         return holder;
     }
 
-
     @Override
-    public void onBindViewHolder(NoteAdapter.ViewHolder holder, int position) {
-        final Note note = notes.get(position);
-        holder.bindNote(note);
-
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Note note = notes.get(position);
+        List<NoteItem> items = note.getItems();
+        holder.tvTitle.setText(note.getTitle());
+        int itemsSize = items.size();
+        if (!items.isEmpty()) {
+            holder.tvBody.setText(items.get(0).getText());
+//            for (int i = 0; i < itemsSize; i++) {
+//                String text = items.get(i).getText();
+//                if (!text.isEmpty()) {
+//                    holder.tvBody.append(text);
+//                    if (items.get(i).getDone() != 0) {
+//                        holder.tvBody.setPaintFlags(holder.tvBody.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+//                    }
+//
+//                    if (i < itemsSize - 1) {
+//                        holder.tvBody.append("\n");
+//                    }
+//                }
+//            }
+        }
     }
-
 
     @Override
     public int getItemCount() {
@@ -59,63 +68,17 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
 
     static class ViewHolder extends RecyclerView.ViewHolder {
 
-        LinearLayout layout;
-        CardView cardView;
-        TextView tvTitle;
-        TextView tvBody;
-        long noteId;
+        private TextView tvTitle;
+        private TextView tvBody;
+        private CardView cardView;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            layout = (LinearLayout) itemView.findViewById(R.id.ll_note_item);
-            cardView = (CardView) itemView.findViewById(R.id.cv_note_item);
+
             tvTitle = (TextView) itemView.findViewById(R.id.tv_note_title);
             tvBody = (TextView) itemView.findViewById(R.id.tv_note_body);
-        }
-
-        public void bindNote(Note note) {
-            noteId = note.getId();
-            String title = note.getTitle();
-            if (title.isEmpty()) {
-                tvTitle.setVisibility(View.GONE);
-            } else {
-                tvTitle.setVisibility(View.VISIBLE);
-                tvTitle.setText(title);
-                tvTitle.setTypeface(null, Typeface.BOLD);
-            }
-            List<NoteItem> items = note.getItems();
-            int itemsSize = items.size();
-            if (!items.isEmpty()) {
-                for (int i = 0; i < itemsSize; i++) {
-                    String text = items.get(i).getText();
-                    if (!text.isEmpty()) {
-                        tvBody.setVisibility(View.VISIBLE);
-                        tvBody.append(text);
-                        if (items.get(i).getDone() != 0) {
-                            tvBody.setPaintFlags(tvBody.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-                        }
-
-                        if (i < itemsSize - 1) {
-                            tvBody.append("\n");
-                        }
-                    }
-
-                }
-
-            }
-
-            if (tvBody.getText().toString().isEmpty()) {
-                tvBody.setVisibility(View.GONE);
-            }
-
-            cardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(itemView.getContext(), NoteActivity.class);
-                    intent.putExtra(itemView.getContext().getResources().getString(R.string.key_note_id), noteId);
-                    itemView.getContext().startActivity(intent);
-                }
-            });
+            cardView = (CardView) itemView.findViewById(R.id.cv_note_item);
         }
     }
+
 }
